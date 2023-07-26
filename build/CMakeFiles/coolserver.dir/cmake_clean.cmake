09214c7fa4befdf30afb35cc58684caec77a1510
Cyan <lancellq@gmail.com>
2023-07-26 13:04:35 -0700

    oh super

---
+++ build/CMakeFiles/coolserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/libcoolserver.pdb"
+  "../lib/libcoolserver.so"
+  "CMakeFiles/coolserver.dir/coolserver/log.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coolserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
