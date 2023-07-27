a7e6cc6f2557ac9a2116f37019d963da1be05dd9
linqingli <lancellq@gmail.com>
2023-07-27 13:20:05 -0700

    config-start3

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,8 +1,13 @@
 #include "../coolserver/config.h"
 #include "../coolserver/log.h"
-
+#include <yaml-cpp/yaml.h>
 coolserver::ConfigVar<int>::ptr g_int_value_config =
     coolserver::Config::Lookup("system.port", (int)8080, "system port");
+
+void test_yaml(){
+    YAML::Node root = YAML::LoadFile("/home/lance/Desktop/server-demo/bin/conf/log.yml");
+    COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) <<root;
+}   
 int main(int argc, char** argv){
     COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << g_int_value_config -> getValue();
     COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << g_int_value_config -> toString();
