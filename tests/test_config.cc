6de3a76ce6a456ca19a3833034b1e079683efb0c
linqingli <lancellq@gmail.com>
2023-07-27 14:29:11 -0700

    config-start6

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -4,9 +4,33 @@
 coolserver::ConfigVar<int>::ptr g_int_value_config =
     coolserver::Config::Lookup("system.port", (int)8080, "system port");
 
+void print_yaml(const YAML::Node& node, int level) {
+    if(node.IsScalar()) {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+            << node.Scalar() << " - " << node.Type() << " - " << level;
+    } else if(node.IsNull()) {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+            << "NULL - " << node.Type() << " - " << level;
+    } else if(node.IsMap()) {
+        for(auto it = node.begin();
+                it != node.end(); ++it) {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                    << it->first << " - " << it->second.Type() << " - " << level;
+            print_yaml(it->second, level + 1);
+        }
+    } else if(node.IsSequence()) {
+        for(size_t i = 0; i < node.size(); ++i) {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                << i << " - " << node[i].Type() << " - " << level;
+            print_yaml(node[i], level + 1);
+        }
+    }
+}
+
 void test_yaml(){
     YAML::Node root = YAML::LoadFile("/home/lance/Desktop/server-demo/bin/conf/log.yml");
-    COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) <<root;
+    print_yaml(root,0);
+    // COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) <<root;
 }   
 int main(int argc, char** argv){
     COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << g_int_value_config -> getValue();
