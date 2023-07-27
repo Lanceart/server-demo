374709a37ab7cce982a19fd3f7a91fdc03a571f3
linqingli <lancellq@gmail.com>
2023-07-27 14:29:53 -0700

    config-start7

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -6,21 +6,21 @@ coolserver::ConfigVar<int>::ptr g_int_value_config =
 
 void print_yaml(const YAML::Node& node, int level) {
     if(node.IsScalar()) {
-        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+        COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << std::string(level * 4, ' ')
             << node.Scalar() << " - " << node.Type() << " - " << level;
     } else if(node.IsNull()) {
-        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+        COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << std::string(level * 4, ' ')
             << "NULL - " << node.Type() << " - " << level;
     } else if(node.IsMap()) {
         for(auto it = node.begin();
                 it != node.end(); ++it) {
-            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+            COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << std::string(level * 4, ' ')
                     << it->first << " - " << it->second.Type() << " - " << level;
             print_yaml(it->second, level + 1);
         }
     } else if(node.IsSequence()) {
         for(size_t i = 0; i < node.size(); ++i) {
-            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+            COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << std::string(level * 4, ' ')
                 << i << " - " << node[i].Type() << " - " << level;
             print_yaml(node[i], level + 1);
         }
