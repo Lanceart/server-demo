2980230a1508088043c378cd9e2d7e3a313dbd12
linqingli <lancellq@gmail.com>
2023-07-27 23:59:52 -0700

    config-start10

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -34,10 +34,13 @@ void test_yaml(){
 }
 
 void test_config() {
-    YAML::Node root = YAML::LoadFile("/home/lance/Desktop/server-demo/bin/conf/log.yml");
-    coolserver::Config::LoadFromYaml(root);
     COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
     COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << "before: " << g_int_value_config->toString();
+    YAML::Node root = YAML::LoadFile("/home/lance/Desktop/server-demo/bin/conf/log.yml");
+    coolserver::Config::LoadFromYaml(root);
+    print_yaml(root,0);
+    COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
+    COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << "after: " << g_int_value_config->toString();
 }
 int main(int argc, char** argv){
     COOLSERVER_LOG_INFO(COOLSERVER_LOG_ROOT()) << g_int_value_config -> getValue();
